c3fb348bc77f27e0f7ae474cc3674bc7c1c8235f
Ameya <ameyavi@gmail.com>
2016-11-12 14:00:18 +0530

    trivia

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'webhook' => 'welcome#verify'
   post 'webhook' => 'welcome#webhook'
 
+  get 'trivia' => 'welcome#solartrivia'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
