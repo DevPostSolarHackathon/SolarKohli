2558f2712a04769d7976bc33e2fd58e6786317d6
Arpit Kulshrestha <Arpit_Kulshrestha@external.mckinsey.com>
2016-11-12 12:08:17 +0530

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  mount Facebook::Messenger::Server, at: "bot"
 
+
+  root 'welcome#index'
   get 'webhook' => 'welcome#verify'
   post 'webhook' => 'welcome#webhook'
 
